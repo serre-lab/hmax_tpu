fe04364b27af3a898899d648cc36e6501d14eead
c-rbp <crbpproject@gmail.com>
2021-04-11 20:18:07 -0400

    New try at multiscale.

--- models/resnet_model_multiscale.py
+++ models/resnet_model_multiscale.py
@@ -104,6 +104,10 @@ def multiscale(
     pool_size=kernel,
     strides=stride,
     data_format=data_format)
+  if data_format == "channels_first":
+    outputs = tf.squeeze(outputs, 2)
+  else:
+    outputs = tf.squeeze(outputs, 1)
   return outputs
 
 
