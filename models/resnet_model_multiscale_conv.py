c157a6502304a3b13928b43dd647bb4e2228ab63
c-rbp <crbpproject@gmail.com>
2021-04-12 20:25:42 -0400

    Fixed conv3d.

--- models/resnet_model_multiscale_conv.py
+++ models/resnet_model_multiscale_conv.py
@@ -129,12 +129,6 @@ def multiscale(
               padding='SAME',
               name='{}_merge'.format(name),
               data_format=data_format))
-  return tf.layers.conv2d(
-      inputs=inputs, filters=filters, kernel_size=kernel_size, strides=strides,
-      padding=('SAME' if strides == 1 else 'VALID'), use_bias=False,
-      kernel_initializer=tf.variance_scaling_initializer(),
-      data_format=data_format, name=name)
-
       if data_format == "channels_first":
           outputs = tf.squeeze(outputs, 2)
       else:
