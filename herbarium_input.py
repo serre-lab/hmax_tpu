2002b9f3ded297fba64d69ff29d239825124450f
piperod <ivan.rodriguez5@upr.edu>
2021-07-22 22:09:06 -0400

    fixing herbarium

--- herbarium_input.py
+++ herbarium_input.py
@@ -1,5 +1,3 @@
-"""https://www.kaggle.com/atamazian/herb-2021-tfrecords-effnet-training"""
-
 # Copyright 2018 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
