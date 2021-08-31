5f5216c76facd6413000b5235f6f7ac75276b2fe
piperod <ivan.rodriguez5@upr.edu>
2021-08-31 09:21:07 -0500

    adding testing files

--- main_herbarium.py
+++ main_herbarium.py
@@ -24,12 +24,21 @@ from hyperparameters import flags_to_params
 from hyperparameters import params_dict
 from configs import resnet_config
 
+def count_data_items(filenames):
+    # the number of data items is written in the name of the .tfrec files, i.e. flowers00-230.tfrec = 230 data items
+    n = [int(re.compile(r"-([0-9]*)\.").search(filename).group(1)) for filename in filenames]
+    return np.sum(n)
 
 common_tpu_flags.define_common_tpu_flags()
 common_hparams_flags.define_common_hparams_flags()
 FLAGS = flags.FLAGS
 #import efficientnet.tfkeras as efn
-
+class CFG:
+    N_CLASSES = 64500
+    IMAGE_SIZE = [256, 256]
+    EPOCHS = 25
+    BATCH_SIZE = 16 * 4#strategy.num_replicas_in_sync
+    
 flags.DEFINE_string(
     'export_dir',
     default=None,
@@ -38,18 +47,13 @@ flags.DEFINE_string(
 AUTO = tf.data.experimental.AUTOTUNE
 TRAINING_FILENAMES =  tf.io.gfile.glob('gs://serrelab/prj-fossil/data/herbarium/train/*.tfrec')
 VALIDATION_FILENAMES =  tf.io.gfile.glob('gs://serrelab/prj-fossil/data/herbarium/validation/*.tfrec')
-#tpu_name=os.getenv('TPU_NAME')
-
-#cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver(
-#tpu=tpu_name)
-
-
-class CFG:
-    N_CLASSES = 64500
-    IMAGE_SIZE = [256, 256]
-    EPOCHS = 25
-    BATCH_SIZE = 16 * 4#strategy.num_replicas_in_sync
-
+TESTING_FILENAMES = tf.io.gfile.glob('gs://serrelab/prj-fossil/data/herbarium/test/*.tfrec')
+NUM_TRAINING_IMAGES = count_data_items(TRAINING_FILENAMES)
+STEPS_PER_EPOCH = NUM_TRAINING_IMAGES//CFG.BATCH_SIZE
+NUM_TESTING_IMAGES = count_data_items(TESTING_FILENAMES)
+TEST_STEPS = NUM_TESTING_IMAGES//CFG.BATCH_SIZE
+NUM_VALIDATION_IMAGES = count_data_items(VALIDATION_FILENAMES)
+VALIDATION_STEPS = NUM_VALIDATION_IMAGES//CFG.BATCH_SIZE
 
 
 def decode_image(image_data):
@@ -125,6 +129,14 @@ def get_validation_dataset():
     dataset = dataset.prefetch(AUTO) # prefetch next batch while training (autotune prefetch buffer size)
     return dataset
 
+def get_testing_dataset():
+    dataset = load_dataset(TESTING_FILENAMES,labeled=False,ordered=True)
+    dataset = dataset.map(onehot, num_parallel_calls=AUTO)
+    dataset = dataset.batch(CFG.BATCH_SIZE)
+    #dataset = dataset.cache()
+    dataset = dataset.prefetch(AUTO) # prefetch next batch while training (autotune prefetch buffer size)
+    return dataset
+
 def count_data_items(filenames):
     # the number of data items is written in the name of the .tfrec files, i.e. flowers00-230.tfrec = 230 data items
     n = [int(re.compile(r"-([0-9]*)\.").search(filename).group(1)) for filename in filenames]
@@ -175,7 +187,7 @@ def main(unused_argv):
     
     params = flags_to_params.override_params_from_input_flags(params, FLAGS)
     # Save params for transfer to GCS
-    np.savez('params.npz', **params.as_dict())
+    np.savez( os.path.join(MAIN_CKP_DIR,'params.npz'), **params.as_dict())
     
     params.validate()
     params.lock()
@@ -197,7 +209,7 @@ def main(unused_argv):
     tf.tpu.experimental.initialize_tpu_system(cluster_resolver)
     strategy = tf.distribute.TPUStrategy(cluster_resolver)
     print("Number of accelerators: ", strategy.num_replicas_in_sync)
-    STEPS_PER_EPOCH=7500
+    
     with strategy.scope():
         # NasNET
         for arch in ['Resnet50v2','Nasnet']:
@@ -218,6 +230,12 @@ def main(unused_argv):
                                                           save_best_only=True,
                                                           save_weights_only=True, 
                                                           mode='min')
+                try: 
+                    print('loading weights from file %s'%ckpt_file)
+                    model.load(ckpt_file)
+                except:
+                    print('loading failed, starting from scratch')
+                    
                 history = model.fit(
                             get_training_dataset(), 
                             steps_per_epoch=STEPS_PER_EPOCH,
@@ -231,6 +249,28 @@ def main(unused_argv):
                 else: 
                     model.save_weights(MAIN_CKP_DIR+'%s_%s_last.h5'%(arch,weights))
 
+            
+            cmdataset = get_validation_dataset(ordered=True) # since we are splitting the dataset and iterating separately on images and labels, order matters.
+            images_ds = cmdataset.map(lambda image, label: image)
+            labels_ds = cmdataset.map(lambda image, label: label).unbatch()
+            cm_correct_labels = next(iter(labels_ds.batch(NUM_VALIDATION_IMAGES))).numpy() # get everything as one batch
+            cm_probabilities = model.predict(images_ds, steps=VALIDATION_STEPS)
+            cm_predictions = np.argmax(cm_probabilities, axis=-1)
+            print("Correct   labels: ", cm_correct_labels.shape, cm_correct_labels)
+            print("Predicted labels: ", cm_predictions.shape, cm_predictions)
+                
+            test_ds = get_testing_dataset() # since we are splitting the dataset and iterating separately on images and ids, order matters.
+            print('Computing predictions...')
+            test_images_ds = test_ds.map(lambda image, idnum: image)
+            probabilities = model.predict(test_images_ds, steps=TEST_STEPS)
+            predictions = np.argmax(probabilities, axis=-1)
+            print(predictions)
+    
+            print('Generating submission.csv file...')
+            test_ids_ds = test_ds.map(lambda image, idnum: idnum).unbatch()
+            test_ids = next(iter(test_ids_ds.batch(NUM_TESTING_FILENAMES))).numpy().astype('U') # all in one batch
+            np.savetxt('%s_submission.csv'%ckpt_file[:-3], np.rec.fromarrays([test_ids, predictions]), fmt=['%s', '%d'], delimiter=',', header='id,label', comments='')
+        
 if __name__ == '__main__':
   #tf.logging.set_verbosity(tf.logging.INFO)
   app.run(main)
